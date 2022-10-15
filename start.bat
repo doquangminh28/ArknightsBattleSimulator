2ea736ce137e61adcd59e562cd0558dd63cf5178
doquangminh28 <doquangminh28@gmail.com>
2022-10-15 09:52:50 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,5 +2,5 @@
 @title ArknightsBattleSimulator
 
 @REM py -m venv env && env\scripts\activate.bat && pip install -r requirements.txt -U && ^
-start cmd.exe /c "@title Arknights Crisis Simulator - mitmdump && mitmdump.exe -s ak.py" && ^
+start cmd.exe /c "@title Arknights Crisis Simulator - mitmdump && mitmweb.exe -s ak.py" && ^
 cls && py main.py
